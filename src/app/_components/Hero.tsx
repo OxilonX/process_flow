eeb5f79914aabecca777548c863e1f826feedb2d
Boulmehad Abderrahmane <boabmeuni01@gmail.com>
2026-02-14 13:06:40 +0100

    feat: update dependencies and add new UI components
    - Added new dependencies: class-variance-authority, clsx, lucide-react, radix-ui, shadcn, and tailwind-merge.
    - Updated globals.css to include new color variables and dark mode styles.
    - Modified layout.tsx to set the default HTML class to "dark".
    - Refactored page.tsx to simplify the structure and prepare for new components.
    - Created a new components.json file for shadcn configuration.
    - Added Hero component with placeholder content and buttons.
    - Implemented a reusable Button component with variants and styles.
    - Added utility function for class name merging in utils.ts.

---
+++ src/app/_components/Hero.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+export default function Hero() {
+  return (
+    <div>
+      <div className="flex flex-col item-center">
+        <div>
+          <h1></h1>
+          <p></p>
+        </div>
+        <div>
+          <Button>Try it out</Button>
+          <Button>Learn more</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
